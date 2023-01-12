dae93157eacd7a4e406f3028d66fc8ff1d17be8d
Lucas Cavataio <lucascavataio@gmail.com>
2023-01-12 10:20:59 +0100

    Initial Commit

---
+++ MySchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySchedule
+//
+//  Created by Apps2M on 12/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
